f78a2125cd6424141817ba75cd5cdddd5925be7f
Alejandro Cornejo <acornejovila@gmail.com>
2013-11-19 08:23:05 +0100

    adding server file

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+      app = express(),
+      server = require('http').createServer(app),
+      io = require('socket.io').listen(server),
+      messages = [],
+      sockets = [];
+
+app.use( express.static(__dirname + '/public'));
+
+server.listen(3000);
+
+io.sockets.on('connection', function (socket) {
+
+  sockets.push(socket);
+
+  socket.emit('messages-available', messages);
+
+});
